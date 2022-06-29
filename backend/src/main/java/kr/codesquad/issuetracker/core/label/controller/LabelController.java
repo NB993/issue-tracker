37563551270753439ccf5a1baf0551b278049d08
Jaehong Choe <qnalsfj@gmail.com>
2022-06-29 16:33:14 +0900

    refactor: 공통 Mapping Url을 클래스 레벨로 통합

--- backend/src/main/java/kr/codesquad/issuetracker/core/label/controller/LabelController.java
+++ backend/src/main/java/kr/codesquad/issuetracker/core/label/controller/LabelController.java
@@ -2,8 +2,10 @@ package kr.codesquad.issuetracker.core.label.controller;
 
 import javax.validation.Valid;
 import kr.codesquad.issuetracker.core.label.dto.CreateLabelRequest;
+import kr.codesquad.issuetracker.core.label.dto.LabelsResponse;
 import kr.codesquad.issuetracker.core.label.service.LabelService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/labels")
 public class LabelController {
 
     private final LabelService labelService;
 
-    @PostMapping("/labels")
+    @PostMapping
     public void createLabel(@RequestBody @Valid CreateLabelRequest createLabelRequest) {
         labelService.createLabel(createLabelRequest);
     }
