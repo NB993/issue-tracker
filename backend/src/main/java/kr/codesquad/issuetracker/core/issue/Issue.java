ad08e75f51ed798f0bc9b25a19f85b7811371d4e
Jaehong Choe <qnalsfj@gmail.com>
2022-06-29 16:03:40 +0900

    feat: Issue 인스턴스 생성을 위한 생성자 및 컬럼 속성 추가

--- backend/src/main/java/kr/codesquad/issuetracker/core/issue/Issue.java
+++ backend/src/main/java/kr/codesquad/issuetracker/core/issue/Issue.java
@@ -11,15 +11,18 @@ import javax.persistence.OneToMany;
 import kr.codesquad.issuetracker.common.BaseTimeEntity;
 import kr.codesquad.issuetracker.core.label.Label;
 import kr.codesquad.issuetracker.core.milestone.Milestone;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Issue extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-    @Column(name = "open_yn")
+    @Column(name = "open_yn", columnDefinition="bit default 1")
     private boolean isOpen;
     @ManyToOne
     private Milestone milestone;
@@ -29,4 +32,12 @@ public class Issue extends BaseTimeEntity {
     private List<Assignee> assignees;
     @OneToMany(mappedBy = "issue")
     private List<Comment> comments;
+
+    public Issue(String title, Milestone milestone, List<Label> labels, List<Assignee> assignees, List<Comment> comments) {
+        this.title = title;
+        this.milestone = milestone;
+        this.labels = labels;
+        this.assignees = assignees;
+        this.comments = comments;
+    }
 }
